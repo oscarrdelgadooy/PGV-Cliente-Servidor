5303257131525a64223df188ebfccf8f712d5f1a
oscarrdelgadooy <oscardelyanes@gmail.com>
2026-04-25 21:53:33 +0100

    feat: add ServerListener thread to receive broadcast updates

---
+++ src/net/salesianos/client/thread/ServerListener.java
@@ -0,0 +1,26 @@
+package net.salesianos.client.thread;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+public class ServerListener extends Thread {
+    private DataInputStream inputStream;
+
+    public ServerListener(DataInputStream inputStream) {
+        this.inputStream = inputStream;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.print("Tu puja -> ");
+            while (true) {
+                String serverMessage = this.inputStream.readUTF();
+                System.out.println("\n[SUBASTA] " + serverMessage);
+                System.out.print("Tu puja -> ");
+            }
+        } catch (IOException e) {
+            System.out.println("[IOE] Conexión cerrada por el servidor.");
+        }
+    }
+}
