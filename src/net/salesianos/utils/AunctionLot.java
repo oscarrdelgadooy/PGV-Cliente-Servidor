8219640a68542d28589b85ba5b1ad757f3b9ebab
oscarrdelgadooy <oscardelyanes@gmail.com>
2026-04-26 15:29:52 +0100

    feat: integrate custom class-themed mock items into AunctionLot

---
+++ src/net/salesianos/utils/AunctionLot.java
@@ -0,0 +1,25 @@
+package net.salesianos.utils;
+
+import net.salesianos.models.Item;
+
+public class AunctionLot {
+    private Item[] inventory;
+    private int actualIndex;
+
+    public AunctionLot() {
+        this.inventory = Constants.MOCK_LOTS;
+        this.actualIndex = 0;
+    }
+
+    public Item getItemActual() {
+        if (actualIndex < inventory.length) {
+            return inventory[actualIndex];
+        }
+        return null;
+    }
+
+    public boolean nextItemBet() {
+        actualIndex++;
+        return actualIndex < inventory.length;
+    }
+}
